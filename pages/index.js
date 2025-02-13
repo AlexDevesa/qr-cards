4c299128a8429474474f3140811b7d91cb05663e
AlexDevesa <alexdvsa@gmail.com>
2025-02-14 00:22:32 +0100

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="text-2xl font-bold">Bienvenido a mi aplicación NFC 🚀</h1>
+    </div>
+  );
+}
